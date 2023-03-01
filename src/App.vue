e022d584846a0012a328877c704fdd81284d04c7
Omar Medhat <omarmedhat2004@gmail.com>
2023-03-02 06:21:28 +0800

    initial

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+import { RouterLink, RouterView } from "vue-router";
+import Navbar from './components/Navbar.vue'
+import Footer from './components/Footer.vue'
+export default {
+  components: { 
+    Navbar,
+    Footer,
+  },
+}
+</script>
+
+<template>
+<div>
+  <Navbar/>
+    <RouterView />
+  <Footer/>
+</div>
+</template>
+
